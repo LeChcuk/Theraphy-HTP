038f3b6fd420a548579dbad814f0913cfdb06353
pjw9412 <pjwts9412@gmail.com>
2022-03-30 21:52:02 +0900

    Trying to get docker to work on Ec2

--- frontend/src/Upload.js
+++ frontend/src/Upload.js
@@ -112,7 +112,7 @@ function Upload() {
                     console.log('ASYNC');
                     console.log(formData);
                     // return await axios.post('http://localhost:3001/', formData);
-                    return await axios.post('http://172.17.0.1/:3001/', formData);
+                    return await axios.post('http://54.209.161.132:3001/', formData);
                 } catch (error) {
                     console.log(error);
                 }
