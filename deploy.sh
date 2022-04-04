9de3dbb7929425bfd371ee6860120807ff0e60c2
pjw9412 <pjwts9412@gmail.com>
2022-04-04 13:53:42 +0900

    remove lfs and adopt S3

--- deploy.sh
+++ deploy.sh
@@ -1,9 +1,11 @@
 #!/usr/bin/env bash
 
-REPOSITORY=/home/ubuntu/Theraphy-HTP
-cd $REPOSITORY
+REPOSITORY=/home/ubuntu/Theraphy-HTP/backend-flask
 
-echo "> 실행"
+cd $REPOSITORY
+aws s3 cp s3://theraphy-htp-bucket/theraphy-htp-project/house3-1.h5 .
+
+cd ..
 docker kill $(docker ps -q)
 docker rm $(docker ps -a -q)
 docker rmi $(docker images -q)
